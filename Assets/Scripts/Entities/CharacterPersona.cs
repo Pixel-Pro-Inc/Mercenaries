d415ae04db43e995878daafef86a688a54263307
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-07-07 10:57:35 +0200

    Template class for Range was made

--- Assets/Scripts/Entities/CharacterPersona.cs
+++ Assets/Scripts/Entities/CharacterPersona.cs
@@ -337,5 +337,149 @@ namespace Assets.Entities
             #endregion
         }
         #endregion
+        #region RangeCharacter Template
+
+        public class RangeTemplate: CharacterPersona, ICardTraits, ICharacterTraits, IRangeTraits
+        {
+            public static RangeTemplate Instance { get; set; }
+            public RangeTemplate()
+            {
+                Instance = this;
+            }
+           
+
+            #region Character variables
+            public string CharacterName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string CharacterDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Health { get { return Health; } set { Health = 28; if (Health > 100) Health = 100; if (Health < 0) Health = 0; } }
+            public int dodge { get { return dodge; } set { dodge = 8; } }
+            public int Speed { get { return Speed; } set { Speed = 6; } }
+            public double CritC { get { return CritC; } set { CritC = 6; } }
+            public int MagicRes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Armour { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Damage
+            {
+                get { return Damage; }
+                set
+                {
+                    if (LowDamageRange == true)
+                    {
+                        Damage = 4;
+                    }
+                    else
+                    {
+                        Damage = 16;
+                    }
+                }
+            }
+
+
+            public int Mana { get { return Mana; } set { if (Mana > 100) Mana = 100; if (Mana < 0) Mana = 0; } }
+            public int Stamina { get { return Stamina; } set { if (Stamina > 100) Stamina = 100; if (Stamina < 0) Stamina = 0; } }
+            public int ExpPoints
+            {
+                get { return ExpPoints; }
+                set
+                {
+                    if (true/*sessionStarted?*/)
+                    {
+                        Instance.ExpPoints += NewEarnedXp;
+                    }
+                    if (Instance.ExpPoints > 1000)
+                    {
+                        Instance.LevelIncrease();
+                        Instance.ExpPoints -= 1000;
+                    }
+                }
+            }
+            public int NewEarnedXp
+            {
+                get { return NewEarnedXp; }
+                set
+                {
+                    if (EarnedXp == true)
+                    {
+                        Instance.NewEarnedXp = NewEarnedXp;
+                    }
+                    else
+                    {
+                        Instance.NewEarnedXp = 0;
+                    }
+                }
+            }
+            public bool EarnedXp
+            {
+                get { return EarnedXp; }
+                set
+                {
+                    if (true)
+                    {
+                        //SessionOver?EarnedXp=false;
+                    }
+                }
+            } //This bool is made true when XPIncrease is fired and should be made of when sessionOver is true
+            public string BriefDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public List<string> NaturalAllies { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public List<string> NaturalEnemies { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool PassiveRangeTraits { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool LowDamageRange { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            #endregion
+            #region Character Methods
+
+            void ICardTraits.passiveTraits()
+            {
+                throw new NotImplementedException();
+            }
+            public void UniqueActiveBuff()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void UniqueActiveDeBuff()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void LevelIncrease()
+            {
+                Instance.ExperienceLevel++;
+                Instance.Health += 8;
+                Instance.dodge += 4;
+                Instance.Speed += 4;
+                Instance.CritC += 2;
+                if (ExperienceLevel>3)
+                {
+                    Instance.MagicRes += 1;
+                    Instance.Armour += 1;
+                }
+                if (LowDamageRange == true) Instance.Damage += 2;
+                else
+                {
+                    Instance.Damage += 3;
+                }
+                //fire levelIncrease animation
+            }
+
+            public void XPIncrease(bool earnXp, int newEarnedXp)
+            {
+                EarnedXp = earnXp;
+                NewEarnedXp = newEarnedXp;
+            }
+
+            public void ActiveBuff()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void ActiveDeBuff()
+            {
+                throw new NotImplementedException();
+            }
+           
+            #endregion
+        }
+        #endregion
+
+
     }
 }
