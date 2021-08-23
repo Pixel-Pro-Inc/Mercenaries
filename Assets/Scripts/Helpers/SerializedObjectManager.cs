4d490ed3ed7101645d7d16d824bbb32f4b98464e
Yewo Theu <yewotheu123456789@gmail.com>
2021-08-23 13:25:21 +0200

    Added serialization

---
+++ Assets/Scripts/Helpers/SerializedObjectManager.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Entities.Character;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SerializedObjectManager
+{
+    //Add File Paths Here
+    public string[] paths = {"mercenaries/CharacterData/", "mercenaries/PurchasesData" }; // Add name to find specific character 
+
+    public void SaveData(object serializedData, string path)
+    {
+        string savePath = Application.persistentDataPath + path;
+
+        var save = serializedData;
+
+        var binaryFormatter = new BinaryFormatter();
+        using (var fileStream = File.Create(savePath))
+        {
+            binaryFormatter.Serialize(fileStream, save);
+        }
+    }
+    public object RetrieveData(string path)
+    {
+        string savePath = Application.persistentDataPath + path;
+        Persona load = new Persona();
+
+        if (File.Exists(savePath))
+        {            
+            var binaryFormatter = new BinaryFormatter();
+            using (var fileStream = File.Open(savePath, FileMode.Open))
+            {
+                load = (Persona)binaryFormatter.Deserialize(fileStream);
+            }
+        }
+
+        return load;
+    }
+}
